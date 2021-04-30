a143699a285d875c62cc391f2b97bc4e569301e5
Shahabaz Khan <shahbaz.9676@gmail.com>
2021-05-01 01:15:57 +0530

    Removed unnecessary code.

--- src/Screens/CategoriesScreen.js
+++ src/Screens/CategoriesScreen.js
@@ -6,41 +6,6 @@ import { CATEGORIES } from "../data/dummy-data";
 import CategoryGridTile from "../components/CategoryGridTile";
 
 const CategoriesScreen = (props) => {
-  let arr1 = [1, 2, 3, 4, 5];
-  let arr2 = [4, 5];
-
-  //1
-  const arr3 = arr1.filter((number) => arr2.some((num) => num === number));
-
-  //2
-  const flag = arr2.some((num) => arr1.includes(num));
-
-  //3
-  const total = arr1.reduce((accu, num) => accu + num);
-
-  //4
-  const arr4 = arr1.map((number) => number * 2);
-
-  //5
-  const arr5 = [];
-  arr1.forEach((number) => arr5.push(number * 3));
-
-  //6
-  const arr6 = arr1.slice(1, 3);
-
-  //7
-  const arr7 = arr1.splice(2, 2, 11, 12);
-
-  //8
-  let arrA = ["one", "two", "three", "four", "five", "six", "seventy"];
-
-  var current = "";
-  arrA.forEach((name) => {
-    current = name.length > current.length ? name : current;
-  });
-
-  console.log(current);
-
   const renderGridItem = (itemData) => {
     return (
       <CategoryGridTile
